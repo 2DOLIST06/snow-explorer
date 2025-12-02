9893039482c89363f0c53263577c77955e3b807c
2DOLIST06 <contact@2dolist.fr>
2025-12-02 13:55:17 -0500

    Update axios.ts

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SKI_API_BASE || "/api",
+  baseURL: process.env.NEXT_PUBLIC_SKI_API_BASE,
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
